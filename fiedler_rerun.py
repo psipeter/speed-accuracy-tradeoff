d9a6ffc989efb27d6d5f6c81d293bb34b49a309d
Peter Duggins <psipeter@gra-login1.graham.sharcnet>
2023-11-29 13:06:40 -0500

    fiedler optimized parameters normal nov29a

--- fiedler_rerun.py
+++ fiedler_rerun.py
@@ -5,14 +5,16 @@ import pandas as pd
 import pickle
 import optuna
 import mysql.connector
+import json
 from network_revised import SequentialPerception, build_network
 
 pid = str(sys.argv[1])
 difficulty = sys.argv[2]
 label = sys.argv[3]
 study_name = f"{pid}_{difficulty}_{label}"
+collect_name = f"{difficulty}_{label}"
 dPs = [0.4, 0.2, 0.1]
-experiment_time = 240
+experiment_time = 60
 dt = 0.001
 perception_seed = 0
 nNeurons = 500
@@ -21,13 +23,14 @@ dt_sample = 0.1
 max_cues = 12
 cue_step = 4
 
-with open(f"data/fiedler_collect_{study_name}.json") as f:
+with open(f"data/fiedler_collect_{collect_name}.json") as f:
     params = json.load(f)
 param = params[pid]
 ramp = param['ramp']
 threshold = param['threshold']
 relative = param['relative']
 columns = ('type', 'id', 'dP', 'trial', 'accuracy', 'cues', 'max_cues')
+dfs = []
 
 for dP in dPs:
     print(f"pid {pid}, dP {dP}")
@@ -51,7 +54,7 @@ for dP in dPs:
         correct = 1 if choice==net.inputs.correct else 0
         cues = min(int(RT/dt_sample)+1, 2*max_cues)
         dfs.append(pd.DataFrame([['model', pid, dP, trial, 100*correct, cues, max_cues]], columns=columns))
-        # print(f"pid {pid}, trial {trial}, dP {dP}, elapsed time {total_time}, cues {cues}, choice {choice}, correct {net.inputs.correct}")
+        print(f"pid {pid}, trial {trial}, dP {dP}, elapsed time {total_time}, cues {cues}, choice {choice}, correct {net.inputs.correct}")
         total_time += RT
         trial += 1
 sim = pd.concat(dfs, ignore_index=True)
