4039c7bf33558ba9cf28f5383ac896473a878e22
psipeter <psipeter@gmail.com>
2024-07-09 16:47:20 -0400

    simplify fiedler optimize and rerun

--- optimize/fiedler_optimize.py
+++ optimize/fiedler_optimize.py
@@ -8,103 +8,80 @@ import mysql.connector
 from scipy.stats import gaussian_kde
 from model import SequentialPerception, build_network
 
-def chi_squared_distance(a,b):
-    distance = 0
-    for i in range(len(a)):
-        if a[i]+b[i]==0:
-            continue
-        else:
-            distance += np.square(a[i] - b[i]) / (a[i]+b[i])
-    return distance
-
-def get_loss(simulated, empirical, dPs, max_cues, cue_step):
-    loss = 0
-    bins = np.arange(0.0, 2*max_cues+cue_step, cue_step)
-    for dP in dPs:
-        cues_sim = simulated.query("dP==@dP")['cues'].to_numpy()
-        cues_emp = empirical.query("dP==@dP")['cues'].to_numpy()
-        hist_cues_sim = np.histogram(cues_sim, bins=bins)[0]
-        hist_cues_emp = np.histogram(cues_emp, bins=bins)[0]
-        normed_hist_cues_sim = hist_cues_sim / len(cues_sim)
-        normed_hist_cues_emp = hist_cues_emp / len(cues_emp)
-        loss += chi_squared_distance(normed_hist_cues_sim, normed_hist_cues_emp)
-    print(f"loss {loss}")
+def get_mean_loss(simulated, empirical):
+    mean_cues_sim = simulated['cues'].mean()
+    mean_cues_emp = empirical['cues'].mean()
+    loss = np.abs(mean_cues_emp - mean_cues_sim)
     return loss
 
-def get_kde_loss(simulated, empirical, dPs, max_cues):
-    loss = 0
+def get_kde_loss(simulated, empirical, max_cues=12):
     eval_points = np.linspace(0, 2*max_cues, 1000)
-    for dP in dPs:
-        cues_sim = simulated.query("dP==@dP")['cues'].to_numpy()
-        cues_emp = empirical.query("dP==@dP")['cues'].to_numpy()
-        # check for zero variance and assign error accordingly
-        unique_sim = simulated.query("dP==@dP")['cues'].unique()
-        unique_emp = empirical.query("dP==@dP")['cues'].unique()
-        if len(unique_sim)==1 or len(unique_emp)==1:
-            mean_emp = np.mean(cues_emp)
-            mean_sim = np.mean(cues_sim)
-            error = np.abs(mean_emp - mean_sim)
-        else:
-            # assign error normally
-            kde_emp = gaussian_kde(cues_emp, bw_method='scott')
-            kde_sim = gaussian_kde(cues_sim, bw_method='scott')
-            estimate_emp = kde_emp.evaluate(eval_points)
-            estimate_sim = kde_sim.evaluate(eval_points)
-            estimate_emp = estimate_emp / np.sum(estimate_emp)
-            estimate_sim = estimate_sim / np.sum(estimate_sim)
-            error = 1000*np.sqrt(np.mean(np.square(estimate_emp - estimate_sim)))
-        loss += error
-    print(f"loss {loss}")
+    cues_sim = simulated['cues'].to_numpy()
+    cues_emp = empirical['cues'].to_numpy()
+    # check for zero variance and assign error accordingly
+    unique_sim = simulated['cues'].unique()
+    unique_emp = empirical['cues'].unique()
+    if len(unique_sim)==1 or len(unique_emp)==1:
+        mean_emp = np.mean(cues_emp)
+        mean_sim = np.mean(cues_sim)
+        loss = np.abs(mean_emp - mean_sim)
+    else:
+        # assign error normally
+        kde_emp = gaussian_kde(cues_emp, bw_method='scott')
+        kde_sim = gaussian_kde(cues_sim, bw_method='scott')
+        estimate_emp = kde_emp.evaluate(eval_points)
+        estimate_sim = kde_sim.evaluate(eval_points)
+        estimate_emp = estimate_emp / np.sum(estimate_emp)
+        estimate_sim = estimate_sim / np.sum(estimate_sim)
+        loss = 1000*np.sqrt(np.mean(np.square(estimate_emp - estimate_sim)))
     return loss
 
 def objective(trial, pid):
     perception_seed = 0
     dt = 0.001
     dt_sample = 0.1
-    experiment_time = 60
+    experiment_time = 240
+    nNeurons = 500 
+    rA = 1.0
+    # optimize using data from moderate difficulty and 12 max cues
+    max_cues = 12
+    dP = 0.2
     # Optuna picks optimized parameters
-    ramp = trial.suggest_float("ramp", 0.5, 2.0, step=0.01)
-    relative = trial.suggest_float("relative", 0.1, 1.0, step=0.01)
+    ramp = trial.suggest_float("ramp", 0.5, 2.5, step=0.01)
+    relative = trial.suggest_float("relative", 0.01, 1.0, step=0.01)
     threshold = trial.suggest_float("threshold", 0.01, 1.0, step=0.01)
-    # We fix the remaining parameters, but save them for our records
-    nNeurons = 500 # trial.suggest_categorical("nNeurons", [500])
-    rA = 1.0 # trial.suggest_categorical("radius", [1.0])
-    max_cues = 12 # trial.suggest_categorical("max_cues", [12])
-    cue_step = 5 # trial.suggest_categorical("cue_step", [5])  # used in loss function for binning data
-    dPs = trial.suggest_categorical("dPs", [[0.2]])  # change this to compute lost over different difficulties
 
     dfs = []
-    for dP in dPs:
-        print(f"dP {dP}, ramp {ramp}, threshold {threshold}, relative {relative}")
-        # run task_trials iterations of the task, measuring simulated reaction times and accuracies
-        inputs = SequentialPerception(dt_sample=dt_sample, seed=perception_seed+int(dP*10), max_cues=max_cues)
-        total_time = 0
-        task_trial = 0
-        while total_time < experiment_time:
-            inputs.create(dP=dP)
-            # net = build_network(inputs, seed=int(pid), ramp=ramp, threshold=threshold, nNeurons=nNeurons, relative=relative)
-            net = build_network(inputs, seed=task_trial, ramp=ramp, threshold=threshold, nNeurons=nNeurons, relative=relative, rA=rA)        
-            sim = nengo.Simulator(net, progress_bar=False)
-            choice = None
-            RT = None
-            while choice==None:
-                sim.run(dt)
-                if np.any(sim.data[net.pAction][-1,:] > 0.01):
-                    choice = np.argmax(sim.data[net.pAction][-1,:])
-                    RT = sim.trange()[-1]
-                if sim.trange()[-1] > 2*max_cues*dt_sample-dt:
-                    choice = np.argmax(sim.data[net.pValue][-1,:])
-                    RT = sim.trange()[-1]
-            acc = 100 if choice==inputs.correct else 0
-            cues = min(int(RT/dt_sample)+1, 2*max_cues)
-            print(f"trial {task_trial}, dP {dP}, elapsed time {total_time}, cues {cues}")
-            dfs.append(pd.DataFrame([['model', pid, dP, task_trial, cues, acc]], columns=('type', 'pid', 'dP', 'trial', 'cues', 'accuracy')))
-            total_time += RT
-            task_trial += 1
+    columns = ['type', 'pid', 'dP', 'trial', 'cues', 'accuracy', 'max_cues']
+    print(f"dP {dP}, ramp {ramp}, threshold {threshold}, relative {relative}")
+    # run task_trials iterations of the task, measuring simulated reaction times and accuracies
+    inputs = SequentialPerception(dt_sample=dt_sample, seed=perception_seed, max_cues=max_cues)
+    total_time = 0
+    task_trial = 0
+    while total_time < experiment_time:
+        inputs.create(dP=dP)
+        net = build_network(inputs, seed=task_trial, ramp=ramp, threshold=threshold, nNeurons=nNeurons, relative=relative, rA=rA)        
+        sim = nengo.Simulator(net, progress_bar=False)
+        choice = None
+        RT = None
+        while choice==None:
+            sim.run(dt)
+            if np.any(sim.data[net.pAction][-1,:] > 0.01):
+                choice = np.argmax(sim.data[net.pAction][-1,:])
+                RT = sim.trange()[-1]
+            if sim.trange()[-1] > 2*max_cues*dt_sample-dt:
+                choice = np.argmax(sim.data[net.pValue][-1,:])
+                RT = sim.trange()[-1]
+        acc = 100 if choice==inputs.correct else 0
+        cues = min(int(RT/dt_sample)+1, 2*max_cues)
+        print(f"trial {task_trial}, dP {dP}, elapsed time {total_time}, cues {cues}")
+        dfs.append(pd.DataFrame([['model', pid, dP, task_trial, cues, acc, max_cues]], columns=columns))
+        total_time += RT
+        task_trial += 1
     simulated = pd.concat(dfs, ignore_index=True)
-    empirical = pd.read_pickle("data/fiedler_trial.pkl").query("id==@pid & max_cues==@max_cues")
-    # loss = get_loss(simulated, empirical, dPs, max_cues, cue_step)
-    loss = get_kde_loss(simulated, empirical, dPs, max_cues)
+    empirical = pd.read_pickle("data/fiedler_trial.pkl").query("id==@pid & max_cues==@max_cues & dP==@dP")
+    loss = get_mean_loss(simulated, empirical)
+    loss = get_kde_loss(simulated, empirical, max_cues)
     return loss
 
 
@@ -112,8 +89,8 @@ if __name__ == '__main__':
 
     pid = int(sys.argv[1])
     label = sys.argv[2]
-    study_name = f"{pid}_{label}"
-    optuna_trials = 200
+    study_name = f"fiedler_{pid}_{label}"
+    optuna_trials = 1000
 
     # objective(None, dPs, pid)
     # raise
--- optimize/fiedler_rerun.py
+++ optimize/fiedler_rerun.py
@@ -3,19 +3,18 @@ import numpy as np
 import nengo
 import pandas as pd
 import pickle
-import optuna
-import mysql.connector
 import json
 from model import SequentialPerception, build_network
 
 pid = str(sys.argv[1])
 label = sys.argv[2]
-study_name = f"{pid}_{label}"
 experiment_time = 60
 dt = 0.001
 perception_seed = 0
 dt_sample = 0.1
-
+amin = 0.1
+nNeurons = 500
+rA = 1.0
 
 with open(f"data/fiedler_optimized_params_{label}.json") as f:
     params = json.load(f)
@@ -23,85 +22,36 @@ param = params[pid]
 ramp = param['ramp']
 threshold = param['threshold']
 relative = param['relative']
-# max_cues = param['max_cues']
-# dPs = param['dPs']
-cue_step = param['cue_step']
-nNeurons = param['nNeurons']
-rA = param['radius']
 
-columns1 = ['type', 'id', 'difficulty', 'trial', 'position', 'cue', 'value', 'fraction_sampled', 'sampled_cues', 'max_cues', 
-        'chosen', 'target', 'accuracy', 'cue_choice_aligned']
-columns2 = ['type', 'id', 'dP', 'trial', 'accuracy', 'cues', 'max_cues']
-dfs1 = []
-dfs2 = []
-
+columns = ['type', 'id', 'dP', 'trial', 'accuracy', 'cues', 'max_cues']
+dfs = []
 for max_cues in [12, 18]:
     for dP in [0.4, 0.2, 0.1]:
-        print(f"pid {pid}, dP {dP}")
-        if dP==0.4: difficulty = 'easy'
-        if dP==0.2: difficulty = 'moderate'
-        if dP==0.1: difficulty = 'hard'
-        inputs = SequentialPerception(seed=perception_seed+int(dP*10), dt_sample=dt_sample, max_cues=max_cues)
+        print(f"dP {dP}, ramp {ramp}, threshold {threshold}, relative {relative}")
+        # run task_trials iterations of the task, measuring simulated reaction times and accuracies
+        inputs = SequentialPerception(dt_sample=dt_sample, seed=perception_seed+int(dP*10), max_cues=max_cues)
         total_time = 0
-        trial = 0
+        task_trial = 0
         while total_time < experiment_time:
             inputs.create(dP=dP)
-            net = build_network(inputs, seed=trial, ramp=ramp, threshold=threshold, nNeurons=nNeurons, relative=relative, rA=rA)
+            net = build_network(inputs, seed=task_trial, ramp=ramp, threshold=threshold, nNeurons=nNeurons, relative=relative, rA=rA)        
             sim = nengo.Simulator(net, progress_bar=False)
             choice = None
             RT = None
             while choice==None:
                 sim.run(dt)
-                if np.any(sim.data[net.pAction][-1,:] > 0.01):
+                if np.any(sim.data[net.pAction][-1,:] > amin):
                     choice = np.argmax(sim.data[net.pAction][-1,:])
                     RT = sim.trange()[-1]
                 if sim.trange()[-1] > 2*max_cues*dt_sample-dt:
                     choice = np.argmax(sim.data[net.pValue][-1,:])
                     RT = sim.trange()[-1]
-            accuracy = 1.0 if choice==net.inputs.correct else 0.0
-            chosen = "L" if choice==0 else "R"
-            target = "L" if net.inputs.correct==0 else "R"
-            first = "L" if inputs.first==0 else "R"
-            # print(choice, RT, dt_sample, max_cues, trial, total_time)
+            acc = 100 if choice==inputs.correct else 0
             cues = min(int(RT/dt_sample)+1, 2*max_cues)
-            Ls = net.inputs.sampled[0]
-            Rs = net.inputs.sampled[1]
-            # print('L', Ls)
-            # print("R", Rs)
-            for p in range(cues):
-                if first=="L":
-                    cue = 'L' if (p % 2 == 0) else 'R'
-                elif first=="R":
-                    cue = 'R' if (p % 2 == 0) else 'L'   
-                if cue == 'R':
-                    value = Rs[int(p/2)]
-                elif cue == 'L':
-                    value = Ls[int(p/2)]
-                fraction_sampled = (p+1) / cues
-                cue_choice_aligned = 0.0
-                if chosen=='R':
-                    if (cue=='R' and value==1) or (cue=='L' and value==0):
-                        cue_choice_aligned += 1
-                    else:
-                        cue_choice_aligned += -1
-                elif chosen=='L':
-                    if (cue=='L' and value==1) or (cue=='R' and value==0):
-                        cue_choice_aligned += 1
-                    else:
-                        cue_choice_aligned += -1
-                # print(p, cue, value, fraction_sampled)
-                df1 = pd.DataFrame([[
-                    'model', pid, difficulty, trial, p, cue, value, fraction_sampled, cues, max_cues,
-                    chosen, target, accuracy, cue_choice_aligned
-                    ]], columns=columns1)
-                dfs1.append(df1)
-
-            dfs2.append(pd.DataFrame([['model', pid, dP, trial, 100*accuracy, cues, max_cues]], columns=columns2))
-            print(f"pid {pid}, trial {trial}, dP {dP}, elapsed time {total_time}, cues {cues}, choice {choice}, correct {net.inputs.correct}")
+            print(f"trial {task_trial}, dP {dP}, elapsed time {total_time}, cues {cues}")
+            dfs.append(pd.DataFrame([['model', int(pid), dP, task_trial, acc, cues, max_cues]], columns=columns))
             total_time += RT
-            trial += 1
+            task_trial += 1
 
-sim1 = pd.concat(dfs1, ignore_index=True)
-sim2 = pd.concat(dfs2, ignore_index=True)
-sim1.to_pickle(f"data/fiedler_position_{pid}_{label}.pkl")
-sim2.to_pickle(f"data/fiedler_trial_{pid}_{label}.pkl")
+simulated = pd.concat(dfs, ignore_index=True)
+simulated.to_pickle(f"data/fiedler_rerun_{pid}_{label}.pkl")
